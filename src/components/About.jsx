06f229c9ce76ed3a1e5c1a9f0e6d82dcd0fb21c3
imReset <fffffreinds101@gmail.com>
2022-03-24 18:37:08 -0700

    fix inline text issue

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -11,15 +11,14 @@ const About = () => {
         </div>
         <div className="max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4">
           <div className="sm:text-right text-4xl font-bold text-[#03045E]">
-            <p>Hi. I'm Clint, nice to meet you. Please take a look around.</p>
-          </div>
-          <div className="grid-rows-1	 text-[#03045E]">
-            <p>
-              I am passionate about building excellent software that improves
-              the lives of those around me. I specialize in creating software
-              for clients ranging from individuals and small-businesses all the
-              way to large enterprise corporations. What would you do if you had
-              a software expert available at your fingertips?
+            <h1 class="w-full text-3xl font-semibold block text-center pl-15 pb-10">
+              Hi Clint, make some noise, Please take a look around
+            </h1>
+            <p class="w-full text-3xl font-semibold block text-center  pb-10 content-center	">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur
+              corrupti delectus inventore officia sequi id cumque deleniti
+              provident. Vel ipsum illo doloribus hic labore culpa dolorum,
+              perspiciatis nulla quisquam atque.
             </p>
           </div>
         </div>
